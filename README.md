110a95ada5ecf4966bbc799d5221c9201aecdaa0
y4bx <y4bx@petrobras.com.br>
2022-10-13 14:06:03 -0300

    Spring Boot API REST construa uma API Aula 3 - Atividade 7 - Utilizando Repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-boot-forum
 Projeto do treinamento Curso de Spring Boot API REST da Alura
+
+Para iniciar o servidor execute o método main da classe ForumApplication
+
+Para visualizar os dados do banco h2 acesse http://localhost:8080/h2-console
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,25 +1,22 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
-	@RequestMapping(""
-			+ "/topicos")
+	@Autowired
+	TopicoRepository topicoRepository;
+	
+	@RequestMapping("/topicos")
 	public List<TopicoDTO> lista(){
-		
-		Topico topico = new Topico(".Duvida","Dúvida com spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDTO.converter(topicoRepository.findAll());
 	}
-
 }
