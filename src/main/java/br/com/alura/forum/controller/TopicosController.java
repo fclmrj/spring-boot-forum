f685bae3c278962ca5dc5310b9c13d34dc6308b2
y4bx <y4bx@petrobras.com.br>
2022-10-14 17:17:43 -0300

    Spring Boot API REST construa uma API Aula 4 - Atividade - 2 Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,19 +3,29 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
-	TopicoRepository topicoRepository;
+	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
 	public List<TopicoDTO> lista(String nomeCurso){
 		
 		if (nomeCurso==null) {
@@ -23,5 +33,12 @@ public class TopicosController {
 		}else {
 			return TopicoDTO.converter(topicoRepository.findByCursoNome(nomeCurso));
 		}
+	}
+	
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+		
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);		
 	}
 }
--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -32,34 +32,18 @@ public class Topico {
 	@ManyToOne
 	private Curso curso;
 	
+	public Topico() {}
+	
+	public Topico(String titulo, String mensagem, Curso curso) {
+		super();
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Topico other = (Topico) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
 	public Long getId() {
 		return id;
 	}
@@ -124,4 +108,28 @@ public class Topico {
 		this.respostas = respostas;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topico other = (Topico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
